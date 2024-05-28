0a0dfbf378fdbaa27edd7c969de05e982bb02792
Eduardo <eduardonp03@gmail.com>
2024-05-28 14:30:51 -0300

    Sexto Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,24 @@
 import { Text, View, Image } from "react-native";
 import { StyleSheet } from "react-native";
 import Home from '../src/screens/Home'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function Index() {
   return (
-    <Home>
-  
-    </Home>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        
+      }}
+      >
+
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false}} />
+
+
+
+      </Stack.Navigator>
   );
 }
