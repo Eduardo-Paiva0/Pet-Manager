1208b5c3db6f461fb8ddd80a19c7e44611a4d21f
Eduardo <eduardonp03@gmail.com>
2024-06-02 13:59:54 -0300

    Decimo Primeiro Commit

--- src/screens/Menu/index.js
+++ src/screens/Menu/index.js
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import { firebase } from '../../services/firebaseConfig'
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import { getDatabase, onValue, ref } from "firebase/database";
+import { getAuth, signOut } from "firebase/auth";
+const db = getDatabase();
+const auth = getAuth();
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import styles from './style'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Schedule({navigation}){
 
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+
+    const recuperarDados = () => {
+        onValue(ref(db, 'users/' + auth.currentUser.uid), (snapshot) => {
+            setNome(snapshot.val().nome)
+            setEmail(snapshot.val().email)
+        });
+    }
+
+    useEffect(() => {
+        recuperarDados();
+    }, [])
+
+    const logoff = () => {
+        signOut(auth).then(() => {
+            navigation.navigate('Login')
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
     return (
         <View style = {styles.screenMenu}>
             <View style = {styles.containerTop}>
-            <Text style = {styles.text}>Usuário:  <Text style = {styles.textHighlight}> Usuário </Text></Text>
-            <Text style = {styles.text}>E-mail: <Text style = {styles.textHighlight}> exemplo@gmail.com</Text></Text> 
+            <Text style = {styles.text}>Usuário:  <Text style = {styles.textHighlight}> {nome} </Text></Text>
+            <Text style = {styles.text}>E-mail: <Text style = {styles.textHighlight}> {email}</Text></Text> 
             <Image style={styles.paws1} source={require('../../../assets/Icons/PawsBg.png')} />
             <View style = {styles.containerLogoff}>
             <TouchableOpacity
                 style={styles.buttonLogOff}
-                onPress={() => navigation.navigate('Login')}
+                onPress={logoff}
             >
             <Text style={styles.textButton}> Sair <MaterialCommunityIcons name="door-open" size={30} /></Text>
             </TouchableOpacity>
--- src/screens/Schedule/index.js
+++ src/screens/Schedule/index.js
@@ -4,15 +4,30 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebas
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import styles from './style'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { getDatabase, onValue, ref } from "firebase/database";
+const db = getDatabase();
+const auth = getAuth();
 
 export default function Schedule(){
+    const [nome, setNome] = useState("")
+
+    const recuperarDados = () => {
+        onValue(ref(db, 'users/' + auth.currentUser.uid), (snapshot) => {
+            setNome(snapshot.val().nome)
+        });
+    }
+
+    useEffect(() => {
+        recuperarDados();
+    }, [])
+
 
     return (
         <View style = {styles.screenSchedule}>
             <View style = {styles.containerTop}>
             <Image style={styles.cat} source={require('../../../assets/Icons/Cat.png')} />
             <Text style={styles.text}>Olá,</Text>
-            <Text style = {styles.textHighlight}>Usuário <Text style={styles.text}>!</Text></Text>
+            <Text style = {styles.textHighlight}>{nome} <Text style={styles.text}>!</Text></Text>
             </View>
 
             <View style = {styles.containerSchedule}>
