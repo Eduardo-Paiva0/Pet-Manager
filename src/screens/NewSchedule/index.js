f5c62b5ff78b67ccf3bf8c23557758ad938149a0
Eduardo <eduardonp03@gmail.com>
2024-06-07 20:06:46 -0300

    Decimo Sexto Commit

--- src/screens/NewSchedule/index.js
+++ src/screens/NewSchedule/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { firebase } from '../../services/firebaseConfig'
 import { getAuth } from 'firebase/auth'
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
@@ -8,16 +8,7 @@ import { getDatabase, onValue, ref, push, set } from "firebase/database";
 const db = getDatabase();
 const auth = getAuth();
 
-
 export default function NewSchedule({navigation}){
-
-  const nameInputRef = useRef(null);
-  const adressInputRef = useRef(null);
-  const phoneInputRef = useRef(null);
-  const procedureInputRef = useRef(null);
-  const petNameInputRef = useRef(null);
-  const petAgeInputRef = useRef(null);
-  const dateInputRef = useRef(null);
 
   const [name, setName] = useState("")
   const [adress, setAdress] = useState("")
@@ -76,95 +67,49 @@ export default function NewSchedule({navigation}){
                 <Text style={styles.alert}>{errorCreateSchedule}</Text>
             )}
       <View style = {styles.topContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Schedule')}>
-            <Text style={styles.textButtonGoBack}><MaterialCommunityIcons name="arrow-left-thin-circle-outline" size={30} />
-              Voltar</Text>
-            </TouchableOpacity>
             <Text style={styles.text}>Faça seu <Text style = {styles.textHighlight}> agendamento </Text>!</Text>
       </View>
         <View style={styles.containerMid}>
           <Text style={styles.textContainer}>Seu nome:</Text>
           <TextInput style={styles.input}
           value={name}
-          ref={nameInputRef}
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            if (adressInputRef.current) {
-                adressInputRef.current.focus();
-            }
-        }}
           onChangeText={setName}
           ></TextInput>
 
           <Text style={styles.textContainer}>Seu Endereço:</Text>
           <TextInput style={styles.input}
-          ref={adressInputRef}
           value={adress}
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            if (phoneInputRef.current) {
-                phoneInputRef.current.focus();
-            }
-        }}
           onChangeText={setAdress}
           ></TextInput>
 
           <Text style={styles.textContainer}>Seu Telefone:</Text>
           <TextInput style={styles.input}
-          ref={phoneInputRef}
           value={phone}
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            if (procedureInputRef.current) {
-                procedureInputRef.current.focus();
-            }
-        }}
           onChangeText={setPhone}
           ></TextInput>
 
           <Text style={styles.textContainer}>Procedimento:</Text>
           <TextInput style={styles.input}
-          ref={procedureInputRef}
           value={description}
-          onSubmitEditing={() => {
-            if (petNameInputRef.current) {
-                petNameInputRef.current.focus();
-            }
-        }}
           onChangeText={setDescription}
           ></TextInput>
 
           <Text style={styles.textContainer}>Nome do Pet:</Text>
           <TextInput style={styles.input2}
-          ref={petNameInputRef}
           value={petname}
-          onSubmitEditing={() => {
-            if (petAgeInputRef.current) {
-              petAgeInputRef.current.focus();
-            }
-          }}
           onChangeText={setPetName}
           ></TextInput>
 
           <Text style={styles.textContainer}>Idade do Pet:</Text>
           <TextInput style={styles.input2}
-          ref={petAgeInputRef}
           value={petage}
-          onSubmitEditing={() => {
-            if (dateInputRef.current) {
-                dateInputRef.current.focus();
-            }
-        }}
           onChangeText={setPetAge}
           ></TextInput>
 
           <Text style={styles.text}>Escolha a data <Text style = {styles.textHighlight}>do agendamento </Text>!</Text>
           <TextInput placeholder='    Ex:. XX/XX/XXXX' style={styles.input3}
-          ref={dateInputRef}
           value={date}
           onChangeText={setDate}
-          returnKeyType="done"
-          onSubmitEditing={validate}
           ></TextInput>
 
         </View>
